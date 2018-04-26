f12784f4ae7b898a110050cab171c9fff4e2f9b7
lmuaims <35021661+lmuaims@users.noreply.github.com>
2018-04-26 10:54:00 -0700

    Update 2018-04-19_backups_restores.sql

--- 2018-04-19_backups_restores.sql
+++ 2018-04-19_backups_restores.sql
@@ -1,1 +1,8 @@
-mysqldump -u aims –p classwork > classwork_2018041901.sql
+-- Backup All Tables in a Specified Database
+mysqldump -u aims -p classwork > classwork_2018041901.sql
+
+-- Backup All Databases
+mysqldump -u aims -p --all-databases > all_databases_2018041901.sql
+
+-- Restore
+mysql -u aims -p sakila_restore_2018041901 < sakila_film_actor_2018041901.sql
